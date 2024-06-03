df47e672a8998af307317f817fe90077d374584c
李江辰 <SpaceCode@foxmail.com>
2024-06-03 17:13:53 +0800

    chore: initialize Vue of ChatTTS

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  htmlWhitespaceSensitivity: 'ignore',
+  vueIndentScriptAndStyle: false,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  jsxSingleQuote: true
+};
