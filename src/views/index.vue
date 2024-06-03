88d3561911ff167671ba7bc596ae3572af203c70
李江辰 <SpaceCode@foxmail.com>
2024-06-03 17:54:05 +0800

    feat: add arco and adjust project structure

---
+++ src/views/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'HomePage'
+});
+</script>
+
+<template>
+  <main>
+    <a-textarea
+      style="width: 500px; height: 300px"
+      placeholder="Please enter something"
+      allow-clear
+      show-word-limit
+      :max-length="{ length: 300, errorOnly: true }"
+    />
+  </main>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,38 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import autoImport from 'unplugin-auto-import/vite';
+import components from 'unplugin-vue-components/vite';
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    autoImport({
+      imports: [],
+      resolvers: [ArcoResolver()]
+    }),
+    components({
+      extensions: ['vue'],
+      resolvers: [
+        ArcoResolver({
+          resolveIcons: false
+        })
+      ]
+    }),
+    vueJsx()
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    root: fileURLToPath(new URL('./', import.meta.url))
   }
 });
